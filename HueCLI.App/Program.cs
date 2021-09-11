5708ccccb6a2984526a79106b702c37e08fead61
Bart Simons <bart@bartsimons.me>
2021-09-11 21:53:52 +0200

    Add HueCLI.Logic and HueCLI.App projects.

---
+++ HueCLI.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HueCLI.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HueCLI.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HueCLI.Logic
+{
+    public class Class1
+    {
+    }
+}
