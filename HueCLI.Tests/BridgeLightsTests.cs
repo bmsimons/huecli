16b078c29eeb1b598761ab44c8996e1117ee68b8
Bart Simons <bart@bartsimons.me>
2021-10-18 20:38:29 +0200

    Add new unit tests

---
+++ HueCLI.Tests/BridgeLightsTests.cs
@@ -0,0 +1,63 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using HueCLI.Logic;
+using System.IO;
+using HueCLI.Logic.Models;
+
+namespace HueCLI.Tests
+{
+    [TestCaseOrderer("HueCLI.Tests.PriorityOrderer", "HueCLI.Tests")]
+    public class BridgeLightsTests {
+        [Fact, TestPriority(1)]
+        public void PrepareDB() {
+            if (File.Exists("huecli.db"))
+            {
+                File.Delete("huecli.db");
+            }
+
+            if (File.Exists("huecli-log.db"))
+            {
+                File.Delete("huecli-log.db");
+            }
+
+            ConfigurationStore store = new ConfigurationStore();
+            store.Dispose();
+
+            Assert.True(File.Exists("huecli.db"));
+        }
+
+        [Fact, TestPriority(2)]
+        public async Task WriteUsernameToDB() {
+            var bridgeLink = new BridgeLink();
+
+            var username = await bridgeLink.Link(Settings.IPAddress, "UnitTest");
+
+            var configurationStore = new ConfigurationStore();
+
+            configurationStore.AddConfiguration(new Configuration
+            {
+                Id = 0,
+                IPAddress = Settings.IPAddress,
+                Username = username,
+                Alias = "UnitTest"
+            });
+
+            var foundConfiguration = configurationStore.GetConfiguration(Settings.IPAddress);
+
+            configurationStore.Dispose();
+
+            Assert.True(foundConfiguration.Username == username);
+        }
+
+        [Fact, TestPriority(3)]
+        public async Task TestLightsGet() {
+            var bridgeLights = new BridgeLights();
+
+            var lights = await bridgeLights.GetLights(Settings.IPAddress);
+            
+            Assert.True(Settings.LightNames.All(a => lights.Values.Select(s => s.Name).Contains(a)));
+            Assert.True(Settings.LightProductNames.All(a => lights.Values.Select(s => s.ProductName).Contains(a)));
+        }
+    }
+}
--- HueCLI.Tests/Settings.cs
+++ HueCLI.Tests/Settings.cs
@@ -3,5 +3,7 @@ namespace HueCLI.Tests
     public static class Settings
     {
         public static string IPAddress = "10.0.1.101";
+        public static string[] LightNames = new string[] { "Hue ambiance spot 1", "Hue ambiance spot 2", "Hue ambiance spot 3" };
+        public static string[] LightProductNames = new string[] { "Hue lightstrip plus", "Hue white lamp" };
     }
 }
