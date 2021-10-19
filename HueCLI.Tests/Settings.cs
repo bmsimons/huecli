19434647cd24b227fc855d5018ee5434de63b579
Bart Simons <bart@bartsimons.me>
2021-10-19 22:44:08 +0200

    Add model support for state on lights.

--- HueCLI.Tests/Settings.cs
+++ HueCLI.Tests/Settings.cs
@@ -2,9 +2,9 @@ namespace HueCLI.Tests
 {
     public static class Settings
     {
-        public static string IPAddress = "10.0.1.101";
+        public static string IPAddress = "192.168.1.83";
         public static string[] LightNames = new string[] { "Hue ambiance spot 1", "Hue ambiance spot 2", "Hue ambiance spot 3" };
         public static string[] LightProductNames = new string[] { "Hue lightstrip plus", "Hue white lamp" };
-        public static int LightToTurnOffAndOn = 3;
+        public static int LightToTurnOffAndOn = 8;
     }
 }
