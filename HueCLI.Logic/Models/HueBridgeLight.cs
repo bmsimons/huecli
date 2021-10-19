8d28d762086179ad0787a79ac396b3b24793ba4b
Bart Simons <bart@bartsimons.me>
2021-10-19 22:44:08 +0200

    Add model support for state on lights.

--- HueCLI.Logic/Models/HueBridgeLight.cs
+++ HueCLI.Logic/Models/HueBridgeLight.cs
@@ -2,6 +2,27 @@ using System.Text.Json.Serialization;
 
 namespace HueCLI.Logic
 {
+    public class HueBridgeLightState
+    {
+        [JsonPropertyName("on")]
+        public bool On { get; set; }
+
+        [JsonPropertyName("reachable")]
+        public bool Reachable { get; set; }
+
+        [JsonPropertyName("bri")]
+        public int Brightness { get; set; }
+
+        [JsonPropertyName("ct")]
+        public int ColorTemperature { get; set; }
+
+        [JsonPropertyName("hue")]
+        public int Hue { get; set; }
+
+        [JsonPropertyName("sat")]
+        public int Saturation { get; set; }
+    }
+
     public class HueBridgeLight
     {
         [JsonPropertyName("type")]
@@ -12,5 +33,8 @@ namespace HueCLI.Logic
 
         [JsonPropertyName("productname")]
         public string ProductName { get; set; }
+
+        [JsonPropertyName("state")]
+        public HueBridgeLightState State { get; set; }
     }
 }
